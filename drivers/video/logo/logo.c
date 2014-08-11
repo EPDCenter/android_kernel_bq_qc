1fe6a2e163764a472f9bda5c7e2237c147d75284
Sagformas <sagformas@gmail.com>
2014-08-11 21:31:23 +0200

    Añadido Logo Engel

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -84,6 +84,14 @@ const struct linux_logo * __init_refok fb_find_logo(int depth)
 		/* Generic Linux logo */
 		logo = &logo_linux_clut224;
 #endif
+
+#ifdef CONFIG_LOGO_CUSTOM_CLUT224
+    /* Custom Center Linux logo */
+    logo = &logo_kernelocengel_clut224;
+#endif
+
+
+
 #ifdef CONFIG_LOGO_PIPO_CLUT224
 		/* Generic Linux logo */
 		logo = &logo_pipo_clut224;
